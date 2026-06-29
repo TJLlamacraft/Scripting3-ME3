3d83e3b0ade2ed11b3d6f4f5b63de3f67f3a3313
TJLlamacraft <135383660+TJLlamacraft@users.noreply.github.com>
2026-06-30 00:01:32 +0200

    Finishedddd?

--- JS/script.js
+++ JS/script.js
@@ -93,16 +93,58 @@ $(document).ready(function () {
     window.location.href = "portfolio.html";
   });
 
-  $("#Project1").click(function () {
-    window.location.href = "portfolio.html"; //will take to the actual project page
+  let exitButton = $(".Exit-Button");
+  let currentProjetPanel = null;
+
+  //Project Panels
+  const ProjectPanels = [
+    $("#Project-Panel-1"),
+    $("#Project-Panel-2"),
+    $("#Project-Panel-3"),
+    $("#Project-Panel-4"),
+    $("#Project-Panel-5"),
+    $("#Project-Panel-6"),
+  ];
+
+  ProjectPanels.forEach((element) => {
+    element.hide();
+  });
+  //gonna do a set active situation. When the img is pressed, the panel opens, if any area outside of the panel is pressed, the panel closes? That or there is a button to close it.
+  //need to make sure that it gets done automatically based on the project name/ number in order or smth, probs using a for loop
+
+  $(".CarouselIMG").click(function (event) {
+    for (let i = 0; i < ProjectPanels.length; i++) {
+      if (event.target.id === `ProjectIMG${i + 1}`) {
+        console.log(`Open project panel ${i + 1}`);
+        currentProjetPanel = ProjectPanels[i];
+        ProjectPanels[i].show();
+        $(".carousel-control").hide();
+        $("#myCarousel").carousel("pause");
+      }
+    }
   });
 
-  $("#Project2").click(function () {
-    window.location.href = "portfolio.html"; //will take to the actual project page
+  exitButton.click(function () {
+    currentProjetPanel.hide();
+    $(".carousel-control").show();
+    $("#myCarousel").carousel({
+      interval: 8000,
+    });
   });
 
-  $("#Project3").click(function () {
-    window.location.href = "portfolio.html"; //will take to the actual project page
+  $("#LinkedInButton").click(() => {
+    window.open(
+      "https://www.linkedin.com/in/tia-leoni-joseph-984243365/",
+      "_blank",
+    );
+  });
+
+  $("#ItchButton").click(() => {
+    window.open("https://tjllemacraft.itch.io/", "_blank");
+  });
+
+  $("#InstaButton").click(() => {
+    window.open("https://www.instagram.com/tjllamacraft/", "_blank");
   });
 
   //#endregion
